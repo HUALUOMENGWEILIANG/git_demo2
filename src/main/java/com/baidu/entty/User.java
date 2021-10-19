51d6c4e6f89e4ac34f167e21eac26da2549b703a
hebin <2965785629@qq.com>
2021-10-19 10:49:14 +0800

    second commit

--- src/main/java/com/baidu/entty/User.java
+++ src/main/java/com/baidu/entty/User.java
@@ -3,5 +3,6 @@ package com.baidu.entty;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello,word!");
     }
 }
